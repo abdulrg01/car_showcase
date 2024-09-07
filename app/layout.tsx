39ddb8bf8925bee8caaf536193fb70e1c3ea95db
abdulrg01 <abdulrg01@gmail.com>
2024-09-07 09:00:02 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='relative'>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
